e77595ce7661efd9e1f5d3d7623214c9e9be53a5
Song Si-young <chunechew@gmail.com>
2023-01-23 21:25:02 +0900

    프론트: 반응형 레이아웃 적용

--- src/main/ui/my-app/pages/_app.tsx
+++ src/main/ui/my-app/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Navbar from "../components/Navbar";
+import "styles/styles.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const client = new QueryClient({
+    defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+        },
+    },
+});
+
+const App: React.FC<AppProps> = ({ Component, pageProps }) => (
+    <QueryClientProvider client={client}>
+        {process.env.NODE_ENV !== 'production' ? <ReactQueryDevtools initialIsOpen={false} /> : null /* 운영 모드가 아니면 화면 좌측 하단에 아이콘이 뜸 */}
+        <Head>
+            <title>MyBooks</title>
+            {/* <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
+        </Head>
+        <>
+            <Navbar />
+            <div className="container">
+                <Component {...pageProps} />
+            </div>
+        </>
+    </QueryClientProvider>
+);
+
+export default App;
