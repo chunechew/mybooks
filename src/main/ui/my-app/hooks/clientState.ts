61e66f4b0295f7908c19606f9dcca12c2e4db297
Song Si-young <chunechew@gmail.com>
2023-01-24 10:15:49 +0900

    React-Query 용 custom hook 수정

--- src/main/ui/my-app/hooks/clientState.ts
+++ src/main/ui/my-app/hooks/clientState.ts
@@ -2,7 +2,10 @@ import { useQuery, useQueryClient } from 'react-query';
 
 export const useSetClientState = (key: string) => {
   const queryClient = useQueryClient();
-  return (state: any) => queryClient.setQueryData(key, state);
+  return (state: any) => {
+    const newState = Object.assign(queryClient.getQueryData(key) as Object, state);
+    return queryClient.setQueryData(key, newState);
+  }
 };
 
 export const useClientValue = (key: string, initialData: any) => {
